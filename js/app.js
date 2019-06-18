97b5440eb60613e2ec1cb12bdf2f08e303a09153
Clark Alms <clark.alms@gmail.com>
2019-06-18 16:47:40 -0500

    adjusted css with materialize and added if else statements for UA

--- js/app.js
+++ js/app.js
@@ -1,18 +1,4 @@
 $(document).ready(function () {
-
-
-
-
-    // $('select#rating-select').on('click', function(){
-    //     let rate = $('#limit-select').val();
-    //     console.log(rate);
-    // });
-    // $('select#limit-select').on('click', function(){
-
-    //     let limit = $("#limit-select").val();
-    //     console.log(limit);
-    // });
-
     let searchedGifs = [];
     var ratingArray = [];
     var limitArray = [];
@@ -28,102 +14,69 @@ $(document).ready(function () {
     });
 
     function displayGifs() {
-
-
-        var searchTerm = $("#gif-search").val().trim();
+        var searchTerm = $("#gif-search").val().trim().replace(/\s/g, '+');
         var rating = ratingArray.join("");
         var limitNum = limitArray.join("");
-        console.log(rating, limitNum, searchedGifs);
         const queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=jZSoz3FNI4W3ORZ3SNp0p2eDIJaGo3bZ&rating=" + rating + "&limit=" + limitNum;
-        console.log(queryURL);
-        // Creating an AJAX call for the specific movie button being clicked
+
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            // Creating a div to hold the movie
-            var gifDiv = $("<div class='gif'>");
-            var title = response.data[0].title;
-            var pOne = $("<p>").text("Title: " + title);
-            gifDiv.append(pOne)
-            // Storing the release year
-            var source = response.data[0].source_tld;
+            console.log(response);
+            for (let i = 0; i < limitNum; i++) {
+                var title = response.data[i].title;
+                var source = response.data[i].source_tld;
+                var rating = response.data[i].rating;
+                var gifURL = response.data[i].images.fixed_width.url;
 
-            // Creating an element to hold the release year
-            var pTwo = $("<p>").text("Source: " + source);
+                var gifDiv = $("<div class='gif col l3 s12'>");
+                var pOne = $("<p>").text("Title: " + title);
+                var pTwo = $("<p>").text("Source: " + source);
+                var pThree = $("<p>").text("Rating: " + rating);
+                var gif = $("<img>").attr("src", gifURL);
 
-            // Displaying the release year
-            gifDiv.append(pTwo);
-
-
-            // Storing the rating data
-            var rating = response.data[0].rating;
-
-            // Creating an element to have the rating displayed
-            var pThree = $("<p>").text("Rating: " + rating);
-
-            // Displaying the rating
-            gifDiv.append(pThree);
-
-            // Retrieving the URL for the image
-            var gifURL = response.data[0].images.fixed_width.url;
-
-            // Creating an element to hold the image
-            var gif = $("<img>").attr("src", gifURL);
-
-            // Appending the image
-            gifDiv.append(gif);
-
-            // Putting the entire movie above the previous movies
-            $("#gifs-view").append(gifDiv);
+                gifDiv.append(pOne);
+                gifDiv.append(pTwo);
+                gifDiv.append(pThree);
+                gifDiv.append(gif);
+                $("#gifs-view").append(gifDiv);
+            };
         });
-
     };
+    // function createHeader() {
+    //     let searchTerm = searchedGifs[searchedGifs.length-1];
+    //     let limitNum = limitArray.join("");
+    //     var searchedGifDiv = $("<div class='title>'");
+    //     var searchedGifHead = $("<h2>").text(`Here are ${limitNum} ${searchTerm} Gifs:`);
+    //     searchedGifDiv.append(searchedGifHead);
+    //     $("#gifs-view").append(searchedGifDiv);
+    // };
 
-    // Function for displaying movie data
     function renderButtons() {
-
-        // Deleting the movies prior to adding new movies
-        // (this is necessary otherwise you will have repeat buttons)
         $("#buttons-view").empty();
-
-        // Looping through the array of movies
         for (var i = 0; i < searchedGifs.length; i++) {
-
-            // Then dynamicaly generating buttons for each movie in the array
-            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
             var newBtn = $("<button>");
-            // Adding a class of movie-btn to our button
             newBtn.addClass("gif-btn");
-            // Adding a data-attribute
             newBtn.attr("data-name", searchedGifs[i]);
-            // Providing the initial button text
             newBtn.text(searchedGifs[i]);
-            // Adding the button to the buttons-view div
             $("#buttons-view").append(newBtn);
         };
     };
-
-    // This function handles events where a movie button is clicked
     $("#search-btn").on("click", function (event) {
         event.preventDefault();
-        // This line grabs the input from the textbox
         var gifSearch = $("#gif-search").val().trim();
-
-        // Adding movie from the textbox to our array
+        if (!gifSearch){
+            alert("Please enter an artist to search for...")
+        } else if (ratingArray.length == 0){
+            alert("Please select a rating to filter by...")
+        } else if (limitArray.length == 0){
+            alert("Please select a number to limit results by...")
+        } else {
         searchedGifs.push(gifSearch);
-
-        // Calling renderButtons which handles the processing of our movie array
-        
         renderButtons();
-        // displayGifs();
-        // ratingArray.length = 0;
-        // limitArray.length = 0;
+        }
     });
-
-    // Adding a click event listener to all elements with a class of "movie-btn"
     $(document).on("click", ".gif-btn", displayGifs);
-
-    // Calling the renderButtons function to display the intial buttons
     renderButtons();
 });
